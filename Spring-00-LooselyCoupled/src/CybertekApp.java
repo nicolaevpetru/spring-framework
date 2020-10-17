433751215cd22160ceb3b55c49ccb8a08fbcc17f
Petru <nicolaevpetru@outlook.com>
2020-10-17 13:11:24 -0400

    PartTimeMentor added in CybertekApp class.

--- Spring-00-LooselyCoupled/src/CybertekApp.java
+++ Spring-00-LooselyCoupled/src/CybertekApp.java
@@ -1,11 +1,14 @@
-import implementation.Mentor;
 import service.FullTimeMentor;
 import service.MentorAccount;
+import service.PartTimeMentor;
 
 public class CybertekApp {
     public static void main(String[] args) {
         FullTimeMentor fullTime = new FullTimeMentor();
+        PartTimeMentor partTime = new PartTimeMentor();
         MentorAccount mentor = new MentorAccount(fullTime);
         mentor.manageAccount();
+        MentorAccount mentor2 = new MentorAccount(partTime);
+        mentor2.manageAccount();
     }
 }
