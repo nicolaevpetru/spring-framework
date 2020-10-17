f437fe5f68cd5d1f8cffe82e85d80cde089aea15
Petru <nicolaevpetru@outlook.com>
2020-10-17 14:59:41 -0400

    Spring container with XML configuration.

--- Spring-01-SpringContainer/src/main/java/CybertekApp.java
+++ Spring-01-SpringContainer/src/main/java/CybertekApp.java
@@ -1,14 +1,25 @@
 
 import interfaces.Mentor;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class CybertekApp {
     public static void main(String[] args) {
-        BeanFactory container = new ClassPathXmlApplicationContext("config.xml");
-        Mentor mentor = (Mentor) container.getBean("fullTimeMentor");
-        mentor.createAccount();
-        mentor = (Mentor) container.getBean("partTimeMentor");
+        // container using "BeanFactory"
+        //BeanFactory container = new ClassPathXmlApplicationContext("config.xml");
+
+        // container using "ApplicationContext"
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+
+        // Mentor mentor = (Mentor) container.getBean("fullTimeMentor");
+//        mentor.createAccount();
+//        mentor = (Mentor) container.getBean("partTimeMentor");
+//        mentor.createAccount();
+
+
+        // without casting
+        Mentor mentor = container.getBean("fullTimeMentor", Mentor.class);
         mentor.createAccount();
     }
 }
