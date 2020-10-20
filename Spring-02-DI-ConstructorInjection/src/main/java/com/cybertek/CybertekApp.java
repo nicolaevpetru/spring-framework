07d83845ab8e10ffcfbb63559d9b0af3d327cecf
Petru <nicolaevpetru@outlook.com>
2020-10-20 19:25:19 -0400

    DI-Constructor Injection.

---
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/CybertekApp.java
@@ -0,0 +1,18 @@
+package com.cybertek;
+
+import com.cybertek.interfaces.Course;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CybertekApp {
+    public static void main(String[] args) {
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+        Course course = container.getBean("java", Course.class);
+        course.getTeachingHours();
+
+        BeanFactory countainer2 = new ClassPathXmlApplicationContext("config.xml");
+        Course course2 = countainer2.getBean("selenium", Course.class);
+        course2.getTeachingHours();
+    }
+}
