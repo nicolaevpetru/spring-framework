e3ab3d444fac2f1d3b8ea52ecbffa459490a44e9
Petru <nicolaevpetru@outlook.com>
2020-10-20 20:50:20 -0400

    Bean Scope.

---
+++ Spring-03-BeanScope-Singleton-Prototype/src/main/java/cybertek/CybertekApp.java
@@ -0,0 +1,14 @@
+package cybertek;
+
+import com.cybertek.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CybertekApp {
+    public static void main(String[] args) {
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+        Course course = container.getBean("java", Course.class);
+        course.getTeachingHours();
+
+    }
+}
