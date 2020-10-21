2da2913382503ab16e1ace9929d1a393d3d75240
Petru <nicolaevpetru@outlook.com>
2020-10-20 22:09:18 -0400

    Tightly Coupled.

--- Spring-00-TightlyCoupled/src/PartTimeMentor.java
+++ Spring-00-TightlyCoupled/src/PartTimeMentor.java
@@ -1,5 +1,5 @@
 public class PartTimeMentor {
     public void createAccount() {
-        System.out.println("Part Time mentor Account is created...");
+        System.out.println("Part Time mentor account is created...");
     }
 }
