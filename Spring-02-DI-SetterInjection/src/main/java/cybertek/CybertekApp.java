dd150e97e6cf0bd7e6e9ed06064163d687430a21
Petru <nicolaevpetru@outlook.com>
2020-10-20 20:24:30 -0400

    DI-Setter Injection.

---
+++ Spring-02-DI-SetterInjection/src/main/java/cybertek/CybertekApp.java
@@ -0,0 +1,15 @@
+package cybertek;
+
+import com.cybertek.interfaces.Course;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CybertekApp {
+    public static void main(String[] args) {
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+        Course course = container.getBean("java", Course.class);
+        course.getTeachingHours();
+
+    }
+}
--- Spring-02-DI-SetterInjection/src/main/java/cybertek/services/Java.java
+++ Spring-02-DI-SetterInjection/src/main/java/cybertek/services/Java.java
@@ -2,16 +2,23 @@ package cybertek.services;
 
 import com.cybertek.interfaces.Course;
 import com.cybertek.interfaces.ExtraSessions;
+import lombok.Data;
 
+@Data
 public class Java implements Course {
+    private ExtraSessions extraSessions;
 
-    //OfficeHours officeHours;
+/*
+    public OfficeHours getOfficeHours() {
+        return officeHours;
+    }
 
-//    public Java(OfficeHours officeHours) {
-//        this.officeHours = officeHours;
-//    }
+    public void setOfficeHours(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
 
-    ExtraSessions extraSessions;
+ */
+
     @Override
     public void getTeachingHours() {
         System.out.println("Weekly Teaching Hours : " + (20 + extraSessions.getHours()));
