58bacad9eec25748c363982b9220f9ebfc24df96
Petru <nicolaevpetru@outlook.com>
2020-10-20 21:21:16 -0400

    Bean LifeCycle.

--- Spring-04-BeanLifeCycle-Init-and-Destroy/src/main/java/cybertek/CybertekApp.java
+++ Spring-04-BeanLifeCycle-Init-and-Destroy/src/main/java/cybertek/CybertekApp.java
@@ -7,12 +7,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class CybertekApp {
     public static void main(String[] args) {
         ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
-        Course course1 = container.getBean("java", Course.class);
-        Course course2 = container.getBean("java", Course.class);
+        Course course = container.getBean("java", Course.class);
+        course.getTeachingHours();
 
-        System.out.println("Pointing to the same object: " + (course1 == course2));
-        System.out.println("Memory location for course1: " + course1);
-        System.out.println("Memory location for course2: " + course2);
-
+        ((ClassPathXmlApplicationContext) container).close();
     }
 }
