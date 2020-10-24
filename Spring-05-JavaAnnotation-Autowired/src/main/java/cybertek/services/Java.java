522962118effc63c6f8f4a0b4f3447724ed5ba2a
Petru <nicolaevpetru@outlook.com>
2020-10-24 11:39:14 -0400

    Java Annotations - Autowired.

--- Spring-05-JavaAnnotation-Autowired/src/main/java/cybertek/services/Java.java
+++ Spring-05-JavaAnnotation-Autowired/src/main/java/cybertek/services/Java.java
@@ -2,13 +2,37 @@ package cybertek.services;
 
 
 import cybertek.interfaces.Course;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+//@AllArgsConstructor
 public class Java implements Course {
 
+    @Autowired
     private OfficeHours officeHours;
 
+/*
+
+    // Constructor Injection
+    @Autowired
+    public Java(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
+
+*/
+
+
+/*
+
+    // Setter Injection
+    @Autowired
+    public void setOfficeHours(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
+
+*/
 
     @Override
     public void getTeachingHours() {
--- Spring-05-JavaAnnotation-Autowired/src/main/java/cybertek/services/OfficeHours.java
+++ Spring-05-JavaAnnotation-Autowired/src/main/java/cybertek/services/OfficeHours.java
@@ -1,7 +1,9 @@
 package cybertek.services;
 
 import cybertek.interfaces.ExtraSessions;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OfficeHours implements ExtraSessions {
     @Override
     public int getHours() {
