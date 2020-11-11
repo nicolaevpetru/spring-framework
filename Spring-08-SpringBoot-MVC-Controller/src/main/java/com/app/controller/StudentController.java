703a7050952883ad68f256e1d2068bfe894a892f
Petru <nicolaevpetru@outlook.com>
2020-11-11 11:12:54 -0500

    SpringBoot MVC - Controller

---
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/app/controller/StudentController.java
@@ -0,0 +1,14 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+    @GetMapping("/show")
+    public String showForm() {
+        return "home";
+    }
+}
