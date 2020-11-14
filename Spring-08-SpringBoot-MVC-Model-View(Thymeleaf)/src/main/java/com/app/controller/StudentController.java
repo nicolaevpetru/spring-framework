9fd8559f1084f4e7d5f4a72102a957412fc8caab
Petru <nicolaevpetru@outlook.com>
2020-11-14 13:23:47 -0500

    SpringBoot MVC - Model - View. POJO added

--- Spring-08-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/app/controller/StudentController.java
+++ Spring-08-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/app/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.app.controller;
 
+import com.app.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,6 +35,10 @@ public class StudentController {
         // print the birthday
         LocalDate birthday = LocalDate.now().minusYears(60);
         model.addAttribute("birthday", birthday);
+
+        Student student = new Student(1, "Mike", "Smith");
+        model.addAttribute("student", student);
+
         return "student/welcome";
     }
 }
