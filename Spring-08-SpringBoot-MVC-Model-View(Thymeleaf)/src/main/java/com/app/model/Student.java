fb11dbd2961773f90edbf63b8611b6640ba21651
Petru <nicolaevpetru@outlook.com>
2020-11-14 13:24:10 -0500

    SpringBoot MVC - Model - View. POJO added

---
+++ Spring-08-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/app/model/Student.java
@@ -0,0 +1,14 @@
+package com.app.model;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+}
