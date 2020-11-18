0ea6128a463304877b3f3d11d79f9c0081eb13bf
Petru <nicolaevpetru@outlook.com>
2020-11-17 19:19:25 -0500

    SpringBoot MVC

---
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/ticketing/model/Mentor.java
@@ -0,0 +1,18 @@
+package com.ticketing.model;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Mentor {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String gender;
+    private boolean graduated;
+    private String batch;
+    private String company;
+}
