2164ebe2159a3ede85cc8f321f660f05da13d767
Petru <nicolaevpetru@outlook.com>
2020-11-17 20:00:37 -0500

    SpringBoot MVC - mentor-register.html updated

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/ticketing/controller/MentorController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/ticketing/controller/MentorController.java
@@ -1,16 +1,24 @@
 package com.ticketing.controller;
 
+import com.ticketing.model.Mentor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
 
     @GetMapping("/register")
-    public String showForm() {
+    public String showForm(Model model) {
 
+        model.addAttribute("mentor", new Mentor());
+        List<String> batchList = Arrays.asList("B7", "B8", "B9", "B10", "B11", "B12");
+        model.addAttribute("batchList", batchList);
         return "mentor/mentor-register";
     }
 
