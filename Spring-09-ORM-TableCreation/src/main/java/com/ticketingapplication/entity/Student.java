38c9a80f2242681e80c9bd52c83146ab24f21e4e
Petru <nicolaevpetru@outlook.com>
2020-12-12 14:07:20 -0500

    ORM - applied @Temporal and @Column annotations in Student POJO

--- Spring-09-ORM-TableCreation/src/main/java/com/ticketingapplication/entity/Student.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/ticketingapplication/entity/Student.java
@@ -1,6 +1,10 @@
 package com.ticketingapplication.entity;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 
 @Entity
 @Table(name = "students")
@@ -17,4 +21,18 @@ public class Student {
     private String email;
     @Transient
     private String city;
+
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+    @Temporal(TemporalType.TIME)
+    private Date birthTime;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date birthDateTime;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate localDate;
+    @Column(columnDefinition = "TIME")
+    private LocalTime localTime;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime localDateTime;
 }
