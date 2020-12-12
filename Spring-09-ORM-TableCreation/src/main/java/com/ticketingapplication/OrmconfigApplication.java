0ae203a6b4711e2c92cfe5ca98013f36d93ef3dc
Petru <nicolaevpetru@outlook.com>
2020-12-12 14:57:13 -0500

    ORM - implemented @PostConstruct in runner class

--- Spring-09-ORM-TableCreation/src/main/java/com/ticketingapplication/OrmconfigApplication.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/ticketingapplication/OrmconfigApplication.java
@@ -1,13 +1,31 @@
 package com.ticketingapplication;
 
+import com.ticketingapplication.entity.Car;
+import com.ticketingapplication.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class OrmconfigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrmconfigApplication.class, args);
-	}
+    @Autowired
+    CarRepository carRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(OrmconfigApplication.class, args);
+    }
+
+    @PostConstruct
+    public void dataInit() {
+        Car c1 = new Car("Audi", "A7");
+        Car c2 = new Car("Volk", "CC");
+        Car c3 = new Car("BMW", "M5");
+        carRepository.save(c1);
+        carRepository.save(c2);
+        carRepository.save(c3);
+
+    }
 }
