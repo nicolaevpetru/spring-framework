ae9746675b1937ecfc0f1bca42e99c08e4c8a704
Petru <nicolaevpetru@outlook.com>
2020-12-12 14:47:10 -0500

    ORM - repository package added

--- Spring-09-ORM-TableCreation/src/main/java/com/ticketingapplication/entity/Car.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/ticketingapplication/entity/Car.java
@@ -1,14 +1,25 @@
 package com.ticketingapplication.entity;
 
+import lombok.*;
+
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "cars")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     private String make;
     private String model;
+
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
 }
---
+++ Spring-09-ORM-TableCreation/src/main/java/com/ticketingapplication/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.ticketingapplication.repository;
+
+import com.ticketingapplication.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+
+}
