60b19f9131284f5174966cde6ab71627653de18c
Petru <nicolaevpetru@outlook.com>
2020-12-16 20:39:18 -0500

    ORM-Mapping-ManyToMany

--- Spring-09-ORM-MApping-ManyToMany/src/main/java/com/ormmanytomany/entity/Post.java
+++ Spring-09-ORM-MApping-ManyToMany/src/main/java/com/ormmanytomany/entity/Post.java
@@ -22,4 +22,9 @@ public class Post {
 
     private String title;
     private String description;
+
+    public Post(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
 }
--- Spring-09-ORM-MApping-ManyToMany/src/main/java/com/ormmanytomany/entity/Tag.java
+++ Spring-09-ORM-MApping-ManyToMany/src/main/java/com/ormmanytomany/entity/Tag.java
@@ -1,4 +1,27 @@
 package com.ormmanytomany.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Setter
+@Getter
 public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Tag(String name) {
+        this.name = name;
+    }
 }
