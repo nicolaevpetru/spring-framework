5fe6472a96d6c52be18e60c60e381c7d505233a4
Petru <nicolaevpetru@outlook.com>
2020-12-19 11:57:54 -0500

    ORM-Lab-CinemaApp - Account and UserRole added

--- Spring-09-ORM-CinemaLab/src/main/java/com/cinemaapp/entity/User.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cinemaapp/entity/User.java
@@ -4,9 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -18,4 +16,14 @@ public class User extends BaseEntity {
     private String email;
     private String password;
     private String username;
+
+    @OneToOne
+    @JoinColumn(name = "account_details_id")
+    private Account account;
+
+    public User(String email, String password, String username) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+    }
 }
---
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cinemaapp/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.cinemaapp.enums;
+
+public enum UserRole {
+    ADMIN, USER;
+}
