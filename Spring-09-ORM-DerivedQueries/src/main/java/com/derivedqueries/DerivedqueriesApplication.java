1bd49933f54bf04d90456d08cb78d2122564fcb9
Petru <nicolaevpetru@outlook.com>
2020-12-22 19:59:57 -0500

    ORM-Derived Queries

--- Spring-09-ORM-DerivedQueries/src/main/java/com/derivedqueries/DerivedqueriesApplication.java
+++ Spring-09-ORM-DerivedQueries/src/main/java/com/derivedqueries/DerivedqueriesApplication.java
@@ -1,13 +1,36 @@
 package com.derivedqueries;
 
+import com.derivedqueries.repository.RegionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class DerivedqueriesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DerivedqueriesApplication.class, args);
-	}
+
+    @Autowired
+    RegionRepository regionRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DerivedqueriesApplication.class, args);
+    }
+
+    @PostConstruct
+    public void testRegions() {
+        System.out.println();
+        System.out.println("------------------ REGION STARTS -----------------");
+        System.out.println("findByCountry : " + regionRepository.findByCountry("Canada"));
+        System.out.println();
+        System.out.println("findDistinctByCountry : " + regionRepository.findByCountry("Canada"));
+        System.out.println();
+        System.out.println("findByCountryContaining : " + regionRepository.findByCountryContaining("United"));
+        System.out.println();
+        System.out.println("findByCountryContainingOrderByCountry : "+regionRepository.findByCountryContainingOrderByCountry("United"));
+        System.out.println("------------------ REGION ENDS -----------------");
+        System.out.println();
+    }
 
 }
--- Spring-09-ORM-DerivedQueries/src/main/java/com/derivedqueries/repository/RegionRepository.java
+++ Spring-09-ORM-DerivedQueries/src/main/java/com/derivedqueries/repository/RegionRepository.java
@@ -1,4 +1,23 @@
 package com.derivedqueries.repository;
 
-public interface RegionRepository {
+import com.derivedqueries.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RegionRepository extends JpaRepository<Region, Integer> {
+
+    // Display all the regions in Canada
+    List<Region> findByCountry(String country);
+
+    // Display all regions in Canada, without duplicates
+    List<Region> findDistinctByCountry(String country);
+
+    // Display all regions with country name includes 'United'
+    List<Region> findByCountryContaining(String country);
+
+    // Display all regions with country name includes 'United' in order
+    List<Region> findByCountryContainingOrderByCountry(String country);
 }
