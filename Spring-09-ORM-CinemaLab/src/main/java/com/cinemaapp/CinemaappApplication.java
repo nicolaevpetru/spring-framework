584bd1b5429242bd406992d24508700e54ed26a4
Petru <nicolaevpetru@outlook.com>
2020-12-27 14:12:47 -0500

    ORM-JPQL-Cinema Lab

--- Spring-09-ORM-CinemaLab/src/main/java/com/cinemaapp/CinemaappApplication.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cinemaapp/CinemaappApplication.java
@@ -1,13 +1,40 @@
 package com.cinemaapp;
 
+import com.cinemaapp.repository.AccountRepository;
+import com.cinemaapp.repository.CinemaRepository;
+import com.cinemaapp.repository.MovieCinemaRepository;
+import com.cinemaapp.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class CinemaappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CinemaappApplication.class, args);
-	}
+    @Autowired
+    AccountRepository accountRepository;
+    @Autowired
+    CinemaRepository cinemaRepository;
+    @Autowired
+    MovieCinemaRepository movieCinemaRepository;
+    @Autowired
+    TicketRepository ticketRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaappApplication.class, args);
+    }
+
+    @PostConstruct
+    public void testAccount() {
+        System.out.println(accountRepository.fetchAdminUsers());
+        System.out.println(cinemaRepository.distinctBYSponsoredName());
+        System.out.println(movieCinemaRepository.countAllByCinemaId(4L));
+        System.out.println(movieCinemaRepository.retrieveAllByLocationName("AMC Empire 25"));
+        System.out.println(ticketRepository.fetchAllTicketsByUserJPQL(4l));
+        System.out.println(ticketRepository.fetchAllTicketsWithRangeDates(LocalDateTime.now().minusDays(25), LocalDateTime.now()));
+        System.out.println(ticketRepository.retrieveAllBySearchCriteria("it"));
+    }
 }
