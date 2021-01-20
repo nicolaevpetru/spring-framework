25f565a803d48ee2af9e8809c85d2e9b0cf29db6
Petru <nicolaevpetru@outlook.com>
2021-01-19 20:44:04 -0500

    Security - Form Authorization

--- Spring-10-Security-FormAuth/src/main/java/com/security/security/SecurityConfiguration.java
+++ Spring-10-Security-FormAuth/src/main/java/com/security/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.security.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserPrincipalDetailsService userPrincipalDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -30,7 +34,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login?logout=true");
+                .logoutSuccessUrl("/login?logout=true")
+                .and()
+                .rememberMe()
+                .tokenValiditySeconds(120)
+                .key("keySecret")
+                .userDetailsService(userPrincipalDetailsService);
     }
 
 
