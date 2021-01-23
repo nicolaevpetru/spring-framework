d1f674c9c64fb7eb14eb223c74cd689e8eeda149
Petru <nicolaevpetru@outlook.com>
2021-01-23 14:32:38 -0500

    Spring REST - HttpVerbs Legacy

---
+++ Spring-11-Rest-HttpVerbs-Legacy/src/main/java/com/rest/service/ProductService.java
@@ -0,0 +1,21 @@
+package com.rest.service;
+
+import com.rest.entity.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface ProductService {
+
+    List<Product> getProducts();
+
+    List<Product> deleteProduct(long id);
+
+    List<Product> updateProduct(long id, Product product);
+
+    List<Product> createProduct(Product product);
+
+    Product getProduct(long id);
+
+}
