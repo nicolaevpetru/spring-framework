b676fdd34fb74e1e448263428f610375562a67fc
Petru <nicolaevpetru@outlook.com>
2021-01-30 14:18:34 -0500

    Web Client (Mono and Flux)

--- Spring-11-Rest-Jeckson/src/main/java/com/jackson/controller/WebFluxController.java
+++ Spring-11-Rest-Jeckson/src/main/java/com/jackson/controller/WebFluxController.java
@@ -5,6 +5,8 @@ import com.jackson.entity.MovieCinema;
 import com.jackson.repository.GenreRepository;
 import com.jackson.repository.MovieCinemaRepository;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
@@ -23,7 +25,7 @@ public class WebFluxController {
         this.genreRepository = genreRepository;
     }
 
-    @GetMapping("/flus-movie-cinemas")
+    @GetMapping("/flux-movie-cinemas")
     public Flux<MovieCinema> readAllCinemaFlux() {
         return Flux.fromIterable(movieCinemaRepository.findAll());
     }
@@ -52,9 +54,56 @@ public class WebFluxController {
         return Mono.just(updatedGenre);
     }
 
-    @DeleteMapping("delete-genre/{id}")
+    @DeleteMapping("/delete-genre/{id}")
     public Mono<Void> deleteGenre(@PathVariable("id") long id) {
         genreRepository.deleteById(id);
         return Mono.empty();
+    }
+
+    //----------------------------------WEBCLIENT EXAMPLES----------------------------
+
+    @GetMapping("/flux")
+    public Flux<MovieCinema> readWithWebClient() {
+        return webClient.get()
+                .uri("/flux-movie-cinemas")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .retrieve().bodyToFlux(MovieCinema.class);
+    }
+
+    @GetMapping("/mono/{id}")
+    public Mono<MovieCinema> readMonoWithWebClient(@PathVariable("id") Long id) {
+        return webClient
+                .get()
+                .uri("/mono-movie-cinema/{id}", id)
+                .retrieve()
+                .bodyToMono(MovieCinema.class);
+    }
+
+    @GetMapping("/mono-rp")
+    public Mono<MovieCinema> readMonoWithWebClientRequestParam(@RequestParam("id") Long id) {
+        return webClient
+                .get()
+                .uri(uriBuilder ->
+                        uriBuilder.path("/mono-movie-cinema")
+                                .queryParam("id", id)
+                                .build())
+                .retrieve().bodyToMono(MovieCinema.class);
+    }
+
+    @PostMapping("/create")
+    public Mono<Genre> createWebClient(@RequestBody Genre genre) {
+        return webClient.post()
+                .uri("/create-genre")
+                .body(Mono.just(genre), Genre.class)
+                .retrieve()
+                .bodyToMono(Genre.class);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Mono<Void> deleteWebClient(@PathVariable("id") Long id) {
+        return webClient.delete()
+                .uri("/delete-genre/{id}", id)
+                .retrieve()
+                .bodyToMono(Void.class);
     }
 }
--- Spring-11-Rest-Jeckson/src/main/java/com/jackson/entity/BaseEntity.java
+++ Spring-11-Rest-Jeckson/src/main/java/com/jackson/entity/BaseEntity.java
@@ -1,12 +1,17 @@
 package com.jackson.entity;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
+@Getter
+@Setter
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
