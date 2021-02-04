8fcfe767f4bbe8234fb288701ccfb7afaacc33ab
Petru <nicolaevpetru@outlook.com>
2021-02-03 20:10:22 -0500

    Rest - Open Api 3.XX

--- Spring-11-Rest-OpenApi3.0/src/main/java/com/openapi/controller/CinemaController.java
+++ Spring-11-Rest-OpenApi3.0/src/main/java/com/openapi/controller/CinemaController.java
@@ -2,6 +2,8 @@ package com.openapi.controller;
 
 import com.openapi.entity.Cinema;
 import com.openapi.repository.CinemaRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/cinema")
+@Tag(name = "Cinema", description = "Cinema endpoints")
 public class CinemaController {
 
     @Autowired
     private CinemaRepository cinemaRepository;
 
     @GetMapping("/read-all")
+    @Operation(summary = "Read all cinemas")
     public List<Cinema> readAll() {
         return cinemaRepository.findAll();
     }
--- Spring-11-Rest-OpenApi3.0/src/main/java/com/openapi/controller/UserController.java
+++ Spring-11-Rest-OpenApi3.0/src/main/java/com/openapi/controller/UserController.java
@@ -2,6 +2,12 @@ package com.openapi.controller;
 
 import com.openapi.entity.User;
 import com.openapi.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +17,20 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@Tag(name = "User", description = "User endpoints")
 public class UserController {
 
     @Autowired
     UserRepository userRepository;
 
     @GetMapping
-    public List<User> readAll() {
-        return userRepository.findAll();
-    }
+    @Operation(summary = "Read all users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = User.class))}),
+            @ApiResponse(responseCode = "400", description ="Something went wrong",content = @Content),
+            @ApiResponse(responseCode = "404", description ="User not found",content = @Content)
+            })
+            public List<User>readAll(){
+            return userRepository.findAll();
+            }
 }
