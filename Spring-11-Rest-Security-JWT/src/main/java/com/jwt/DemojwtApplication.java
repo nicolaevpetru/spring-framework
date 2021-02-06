61d4e5ef03b811957f96224404ce4ccf1e30e9a1
Petru <nicolaevpetru@outlook.com>
2021-02-06 11:01:11 -0500

    Spring Security - JWT

---
+++ Spring-11-Rest-Security-JWT/src/main/java/com/jwt/DemojwtApplication.java
@@ -0,0 +1,13 @@
+package com.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemojwtApplication.class, args);
+	}
+
+}
---
+++ Spring-11-Rest-Security-JWT/src/main/java/com/jwt/entity/BaseEntity.java
@@ -0,0 +1,53 @@
+package com.jwt.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+    @JsonIgnore
+    @Column(name = "create_date", nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @JsonIgnore
+    @Column(name = "create_date", nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedDate;
+
+    @JsonIgnore
+    @Column(name = "user_created", updatable = false)
+    private Integer createUserId;
+
+    @JsonIgnore
+    @Column(name = "user_updated", nullable = false)
+    private Integer updateUserId;
+
+    @PrePersist
+    private void onPersist() {
+        this.createdDate = new Date();
+        this.updatedDate = new Date();
+        this.createUserId = 1;
+        this.updateUserId = 1;
+    }
+
+    @PreUpdate
+    private void onPreUpdate() {
+        this.updatedDate = new Date();
+        this.updateUserId = 1;
+    }
+}
