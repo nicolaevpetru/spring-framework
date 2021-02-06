2cf11dff0a20b7d8d0fd6c1804c80e811b2c9ae7
Petru <nicolaevpetru@outlook.com>
2021-02-06 14:45:22 -0500

    Spring Security - JWT

--- Spring-11-Rest-Security-JWT/src/main/java/com/jwt/config/WebSecurityConfig.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/jwt/config/WebSecurityConfig.java
@@ -1,15 +1,23 @@
 package com.jwt.config;
 
+import com.jwt.service.SecurityFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private SecurityFilter securityFilter;
+
 
     @Override
     @Bean
@@ -29,5 +37,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .anyRequest()
                 .authenticated();
+        http.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
