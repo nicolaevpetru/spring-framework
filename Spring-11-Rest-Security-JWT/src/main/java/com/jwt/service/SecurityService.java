1fc26b8aaa50e0f68198741581f03952603abca8
Petru <nicolaevpetru@outlook.com>
2021-02-06 13:41:31 -0500

    Spring Security - JWT

---
+++ Spring-11-Rest-Security-JWT/src/main/java/com/jwt/service/SecurityService.java
@@ -0,0 +1,41 @@
+package com.jwt.service;
+
+import com.jwt.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SecurityService implements UserDetailsService {
+
+    private UserService userService;
+
+    public SecurityService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User foundUser = loadUser(s);
+        if (foundUser == null) {
+            throw new UsernameNotFoundException("User not found! " + s);
+        }
+        return new org.springframework.security.core.userdetails.User(foundUser.getUsername(), foundUser.getPassword(), listAuthorities(foundUser));
+    }
+
+    private User loadUser(String value) {
+        boolean isEmail = value.contains("@");
+        return isEmail ? userService.readByEmail(value) : userService.readByUsername(value);
+    }
+
+    private List<GrantedAuthority> listAuthorities(User user) {
+        List<GrantedAuthority> grantedAuthoritiesList = new ArrayList<>();
+        grantedAuthoritiesList.add(new SimpleGrantedAuthority(user.getRole().name()));
+        return grantedAuthoritiesList;
+    }
+}
