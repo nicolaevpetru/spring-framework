fcc121335339e345b4091e3576f5297a7c22420e
Petru <nicolaevpetru@outlook.com>
2021-02-27 11:31:47 -0500

    Unit testing - JUnit5

--- Spring-13-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ Spring-13-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -17,8 +17,37 @@ class CalculatorTest {
     }
 
     @Test
+    void testCase3() {
+        assertArrayEquals(new int[]{1, 2, 3}, new int[]{1, 2, 3,});
+    }
+
+    @Test
+    void testCase4() {
+        String nullString = null;
+        String notNullString = "not null String";
+
+        assertNull(nullString);
+//        assertNotNull(nullString);
+        assertNotNull(notNullString);
+    }
+
+    @Test
+    void testCase5() {
+        Calculator c1 = new Calculator();
+        Calculator c2 = c1;
+        Calculator c3 = new Calculator();
+
+        assertNotSame(c1, c3);
+//        assertNotSame(c1, c2);
+        assertSame(c1, c2);
+
+    }
+
+    @Test
     void add() {
         int actual = Calculator.add(2, 3);
         assertEquals(5, actual);
     }
+
+
 }
