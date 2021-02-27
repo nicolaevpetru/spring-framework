b1e30aa13d735c0e2f6503d43ba65a2ed9123185
Petru <nicolaevpetru@outlook.com>
2021-02-27 11:18:36 -0500

    Unit testing - JUnit5

--- Spring-13-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ Spring-13-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -5,7 +5,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @Test
-    void add() {
+    void testCase1() {
+        assertEquals(20, 20);
 
+    }
+
+    @Test
+    void testCase2() {
+//        fail("Not implemented yet");
+        assertTrue(Calculator.operator.equals("add"));
+    }
+
+    @Test
+    void add() {
+        int actual = Calculator.add(2, 3);
+        assertEquals(5, actual);
     }
 }
